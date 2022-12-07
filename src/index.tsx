cd98cbe389d2e34cc6c019d3aafd1edc812c62a4
ap-justin <justin@angelprotocol.io>
2022-12-07 14:25:53 +0800

    update react

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-import { getChainOptions, WalletProvider } from "@terra-money/wallet-provider";
-import ReactDOM from "react-dom";
+import { WalletProvider } from "@terra-money/wallet-provider";
+import { createRoot } from "react-dom/client";
 import { store } from "store";
 import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "app";
+import { initTheme } from "helpers/theme";
+import { chainOptions } from "chainOptions";
 
-getChainOptions().then((chainOptions) => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <WalletProvider {...chainOptions}>
-        <App />
-      </WalletProvider>
-    </Provider>,
-    document.getElementById("root")
-  );
-});
+//set theme immediately, so even suspense loaders and can use it
+initTheme();
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <Provider store={store}>
+    <WalletProvider {...chainOptions}>
+      <App />
+    </WalletProvider>
+  </Provider>
+);
