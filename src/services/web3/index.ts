acb1b9372299b005f66e3f6e30601bec26ae6b83
ap-justin <justin@angelprotocol.io>
2022-12-10 15:42:35 +0800

    default lcd and rpc for chain

--- src/services/web3/index.ts
+++ src/services/web3/index.ts
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { ethers, utils } from "ethers";
-import { Coin, CoinData, CoinWithBalance, FetchedChain } from "types";
+import { Coin, CoinData } from "types";
 import ERC20Abi from "abi/ERC20.json";
 import { EVMContract, JsonRpcProvider } from "types/third-party/ethers";
-import { Chain } from "constants/chains";
+import { chains } from "constants/chains";
 import { queryContract } from "./queryContract";
-import { APIs } from "constants/urls";
+import { condenseToNum } from "helpers/decimal";
 
 export const web3Api = createApi({
   reducerPath: "web3",
@@ -14,113 +14,55 @@ export const web3Api = createApi({
     mode: "cors",
   }),
   endpoints: (builder) => ({
-    balance: builder.query<any, Coin & { chainId: string }>({
-      providesTags: [],
-      async queryFn(args, queryApi, extraOptions, baseQuery) {
-        return { data: undefined };
-      },
-    }),
-    balances: builder.query<
-      CoinWithBalance[],
-      Chain & { id: string; walletAddr: string }
-    >({
-      providesTags: [],
-      async queryFn(args, queryApi, extraOptions, baseQuery) {
-        try {
-          const { tokens, native_currency } = await fetch(
-            `${APIs.apes}/v1/chain/${args.id}`
-          ).then<FetchedChain>((res) => {
-            if (!res.ok) throw new Error("failed to fetch chain");
-            return res.json();
-          });
+    balance: builder.query<number, Coin & { chainId: string; address: string }>(
+      {
+        providesTags: [],
+        async queryFn({ chainId, address, ...coin }) {
+          const chain = chains[chainId];
 
-          /**fetch balances for terra  */
-          if (args.type === "terra") {
-            const { balances: natives } = await fetch(
-              args.lcd + `/cosmos/bank/v1beta1/balances/${args.walletAddr}`
-            ).then<{ balances: CoinData[] }>((res) =>
-              !res.ok ? { balances: [] } : res.json()
-            );
-
-            const cw20s = await getCW20Balance(
-              args.walletAddr,
-              args.lcd,
-              tokens
-            );
-
-            return {
-              data: [native_currency, ...tokens].map((t) => ({
-                ...t,
-                balance: (() => {
-                  const bal =
-                    natives.concat(cw20s).find((b) => b.denom === t.token_id)
-                      ?.amount || "0";
-                  return +utils.formatUnits(bal, t.decimals);
-                })(),
-              })),
-            };
+          switch (coin.type) {
+            case "terra-native":
+            case "ibc": {
+              const { balance } = await fetch(
+                `${chain.lcd}/cosmos/bank/v1beta1/balances/${address}/by_denom?denom=${coin.token_id}`
+              ).then<{ balance: CoinData }>((res) => {
+                if (!res.ok) {
+                  throw new Error(`failed to fetch balance ${coin.token_id}`);
+                }
+                return res.json();
+              });
+              return { data: condenseToNum(balance.amount, coin.decimals) };
+            }
+            case "cw20": {
+              const { balance } = await queryContract(
+                "cw20Balance",
+                coin.token_id,
+                { addr: address },
+                chain.lcd
+              );
+              return { data: condenseToNum(balance, coin.decimals) };
+            }
+            case "erc20": {
+              const provider = new JsonRpcProvider(chain.rpc);
+              const contract = new EVMContract(
+                coin.token_id,
+                ERC20Abi,
+                provider
+              );
+              const balance = contract.balanceOf(
+                address
+              ) as Promise<ethers.BigNumber>;
+              return { data: +utils.formatUnits(await balance, coin.decimals) };
+            }
+            default: /** evm native */
+              const provider = new JsonRpcProvider(chain.rpc);
+              const balance = await provider.getBalance(address);
+              return { data: +utils.formatUnits(balance, coin.decimals) };
           }
-
-          if (args.type === "evm") {
-            const _tokens = [native_currency, ...tokens];
-            const balanceQueries = _tokens.map((t) => {
-              const jsonProvider = new JsonRpcProvider(args.rpc);
-              if (t.type === "erc20") {
-                const contract = new EVMContract(
-                  t.token_id,
-                  ERC20Abi,
-                  jsonProvider
-                );
-                return contract.balanceOf(
-                  args.walletAddr
-                ) as Promise<ethers.BigNumber>;
-              }
-              return jsonProvider.getBalance(args.walletAddr);
-            });
-            const queryResults = await Promise.allSettled(balanceQueries);
-            return {
-              data: queryResults.map((result, i) => ({
-                ..._tokens[i],
-                balance:
-                  result.status === "fulfilled"
-                    ? +utils.formatUnits(result.value, _tokens[i].decimals)
-                    : 0,
-              })),
-            };
-          }
-          return { data: [] };
-        } catch (err) {
-          console.log(err);
-          return {
-            error: {
-              status: 500,
-              statusText: "Query error",
-              data: "Failed to get balances",
-            },
-          };
-        }
-      },
-    }),
+        },
+      }
+    ),
   }),
 });
 
-export const { useBalancesQuery } = web3Api;
-
-async function getCW20Balance(
-  address: string,
-  lcd: string,
-  tokens: Coin[]
-): Promise<CoinData[]> {
-  const cw20BalancePromises = tokens
-    .filter((x) => x.type === "cw20")
-    .map((x) =>
-      queryContract("cw20Balance", x.token_id, { addr: address }, lcd).then(
-        (data) => ({
-          denom: x.token_id,
-          amount: data.balance,
-        })
-      )
-    );
-  const cw20Balances = await Promise.all(cw20BalancePromises);
-  return cw20Balances;
-}
+export const { useBalanceQuery } = web3Api;
